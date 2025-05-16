8349094250267c2a6afe12601009ee2fbb1ac04a
mauradewey <mauradewey@gmail.com>
2025-05-16 14:05:27 +0200

    moving rahul's original scripts out of new project folder, adding bash submission script for tetralith

---
+++ submit_mcmc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#SBACTH -A naiss2024-1-3
+#SBATCH -J CCN_MCMC
+#SBATCH -t 04:00:00
+#SBATCH -N 1
+#SBATCH --exclusive
+#SBATCH --mail-user=maura.dewey@misu.su.se  
+#SBATCH --mail-type=FAIL
+#SBATCH -o logs/slurm-dask-mcmc_%j.out
+#SBATCH -e logs/slurm-dask-mcmc_%j.out
+           
+module load Miniforge/24.7.1-2-hpc1
+conda activate mcmc_env
+python code/main.py
